d7e487ccf233f75748b773cfb3d63368ba824eec
lina-hs <lina.hannersanchez@gmail.com>
2016-08-30 12:18:41 +0200

    Created a basic name application. (Uppgift 0830-1-1)

---
+++ NameApplication/NameApplication/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NameApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Get first name
+            Console.WriteLine("What is your first name?");
+            string firstname = Console.ReadLine();
+
+            // Get last name
+            Console.WriteLine("What is your last name?");
+            string lastname = Console.ReadLine();
+
+            Console.WriteLine("Hey " + firstname + " " + lastname);
+
+
+        }
+    }
+}
