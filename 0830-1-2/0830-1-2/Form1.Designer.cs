de56a71db588352bde1177c8fcd92a4df1139a9c
lina-hs <lina.hannersanchez@gmail.com>
2016-08-30 13:42:03 +0200

    Added function StringCapFirst() and two conditions for the messagebox.
    First condition: The user must enter a name.
    Second condition: The name of the user cannot be equal to the default
    value of the textbox.
    
    StringCapFirst(): Makes the first character of a string to uppercase,
    does not take into consideration numbers/symbols or characters differing
    to letters.

--- 0830-1-2/0830-1-2/Form1.Designer.cs
+++ 0830-1-2/0830-1-2/Form1.Designer.cs
@@ -31,20 +31,21 @@
             this.boxLastName = new System.Windows.Forms.TextBox();
             this.btnHello = new System.Windows.Forms.Button();
             this.boxFirstName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // boxLastName
             // 
-            this.boxLastName.Location = new System.Drawing.Point(172, 174);
+            this.boxLastName.Location = new System.Drawing.Point(150, 174);
             this.boxLastName.Name = "boxLastName";
             this.boxLastName.Size = new System.Drawing.Size(166, 20);
             this.boxLastName.TabIndex = 1;
-            this.boxLastName.Text = "Enter your last name";
             this.boxLastName.TextChanged += new System.EventHandler(this.boxLastName_TextChanged);
             // 
             // btnHello
             // 
-            this.btnHello.Location = new System.Drawing.Point(185, 210);
+            this.btnHello.Location = new System.Drawing.Point(163, 200);
             this.btnHello.Name = "btnHello";
             this.btnHello.Size = new System.Drawing.Size(140, 61);
             this.btnHello.TabIndex = 2;
@@ -54,18 +55,37 @@
             // 
             // boxFirstName
             // 
-            this.boxFirstName.Location = new System.Drawing.Point(172, 138);
+            this.boxFirstName.Location = new System.Drawing.Point(150, 126);
             this.boxFirstName.Name = "boxFirstName";
             this.boxFirstName.Size = new System.Drawing.Size(166, 20);
             this.boxFirstName.TabIndex = 3;
-            this.boxFirstName.Text = "Enter your first name";
             this.boxFirstName.TextChanged += new System.EventHandler(this.boxFirstName_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(150, 158);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(103, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Enter your last name";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(150, 110);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(103, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Enter your first name";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(513, 402);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.boxFirstName);
             this.Controls.Add(this.btnHello);
             this.Controls.Add(this.boxLastName);
@@ -80,6 +100,8 @@
         private System.Windows.Forms.TextBox boxLastName;
         private System.Windows.Forms.Button btnHello;
         private System.Windows.Forms.TextBox boxFirstName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- 0830-1-2/0830-1-2/Form1.cs
+++ 0830-1-2/0830-1-2/Form1.cs
@@ -12,26 +12,72 @@ namespace _0830_1_2
 {
     public partial class Form1 : Form
     {
+
+        string defFirstName;
+        string defLastName;
+
         public Form1()
         {
             InitializeComponent();
+
+            defFirstName = boxFirstName.Text;
+            defLastName = boxLastName.Text;
+            initFirstName = false;
+            initLastName = false;
         }
 
         private void boxFirstName_TextChanged(object sender, EventArgs e)
         {
 
         }
-
         private void boxLastName_TextChanged(object sender, EventArgs e)
         {
 
         }
-
         private void btnHello_Click(object sender, EventArgs e)
         {
-            
-            MessageBox.Show("Hey " + boxFirstName.Text + " " + boxLastName.Text);
 
+            bool modFirstName = defFirstName != boxFirstName.Text;
+            bool modLastName = defLastName != boxLastName.Text;
+
+            if (modFirstName && modLastName && boxFirstName.Text != "" && boxLastName.Text != "")
+            {
+                MessageBox.Show("Hey " + StringCapFirst(boxFirstName.Text) + " " + StringCapFirst(boxLastName.Text));
+            }
+            else
+            {
+
+                string showMsg = "";
+
+                if (!modFirstName)
+                {
+                    showMsg += "You must enter a first name.\n";
+                }
+                if (!modLastName)
+                {
+                    showMsg += "You must enter a last name.\n";
+                }
+                if (boxFirstName.Text == "")
+                {
+                    showMsg += "You must enter a first name.\n";
+                }
+                if (boxLastName.Text == "")
+                {
+                    showMsg += "You must enter a last name.";
+                }
+
+                MessageBox.Show(showMsg);
+            }
+
+        }
+
+        private string StringCapFirst(string msg) {
+            /*
+             *  Capitalizes the first character of a string.
+             *  Does not take other factors into consideration, eg, is the character a number/symbol.
+             */
+            msg = Char.ToUpper(msg[0]).ToString() + msg.Substring(1);
+            return msg;
         }
     }
 }
