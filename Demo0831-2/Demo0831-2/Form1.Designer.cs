f552af626e3f83538a2c8fc349cd45a4c3c8c2fc
lina-hs <lina.hannersanchez@gmail.com>
2016-08-31 10:24:19 +0200

    [Demo0831-2] Created a preliminary calculator.

--- Demo0831-2/Demo0831-2/Form1.Designer.cs
+++ Demo0831-2/Demo0831-2/Form1.Designer.cs
@@ -28,12 +28,106 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tbNumber1 = new System.Windows.Forms.TextBox();
+            this.tbNumber2 = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnSub = new System.Windows.Forms.Button();
+            this.btnMulti = new System.Windows.Forms.Button();
+            this.btnDiv = new System.Windows.Forms.Button();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tbNumber1
+            // 
+            this.tbNumber1.Location = new System.Drawing.Point(84, 36);
+            this.tbNumber1.Name = "tbNumber1";
+            this.tbNumber1.Size = new System.Drawing.Size(100, 20);
+            this.tbNumber1.TabIndex = 1;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(97, 210);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(75, 23);
+            this.btnCalculate.TabIndex = 2;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // tbNumber2
+            // 
+            this.tbNumber2.Location = new System.Drawing.Point(84, 62);
+            this.tbNumber2.Name = "tbNumber2";
+            this.tbNumber2.Size = new System.Drawing.Size(100, 20);
+            this.tbNumber2.TabIndex = 3;
+            // 
+            // btnSub
+            // 
+            this.btnSub.Location = new System.Drawing.Point(102, 134);
+            this.btnSub.Name = "btnSub";
+            this.btnSub.Size = new System.Drawing.Size(32, 32);
+            this.btnSub.TabIndex = 4;
+            this.btnSub.Text = "-";
+            this.btnSub.UseVisualStyleBackColor = true;
+            this.btnSub.Click += new System.EventHandler(this.btnSub_Click);
+            // 
+            // btnDiv
+            // 
+            this.btnDiv.Location = new System.Drawing.Point(178, 134);
+            this.btnDiv.Name = "btnDiv";
+            this.btnDiv.Size = new System.Drawing.Size(32, 32);
+            this.btnDiv.TabIndex = 5;
+            this.btnDiv.Text = "/";
+            this.btnDiv.UseVisualStyleBackColor = true;
+            this.btnDiv.Click += new System.EventHandler(this.btnDiv_Click);
+            // 
+            // btnMulti
+            // 
+            this.btnMulti.Location = new System.Drawing.Point(140, 134);
+            this.btnMulti.Name = "btnMulti";
+            this.btnMulti.Size = new System.Drawing.Size(32, 32);
+            this.btnMulti.TabIndex = 6;
+            this.btnMulti.Text = "x";
+            this.btnMulti.UseVisualStyleBackColor = true;
+            this.btnMulti.Click += new System.EventHandler(this.btnMulti_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(64, 134);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(32, 32);
+            this.btnAdd.TabIndex = 7;
+            this.btnAdd.Text = "+";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.btnMulti);
+            this.Controls.Add(this.btnDiv);
+            this.Controls.Add(this.btnSub);
+            this.Controls.Add(this.tbNumber2);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.tbNumber1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        private System.Windows.Forms.TextBox tbNumber1;
+        private System.Windows.Forms.TextBox tbNumber2;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnSub;
+        private System.Windows.Forms.Button btnMulti;
+        private System.Windows.Forms.Button btnDiv;
+        private System.Windows.Forms.Button btnCalculate;
     }
 }
 
--- Demo0831-2/Demo0831-2/Form1.cs
+++ Demo0831-2/Demo0831-2/Form1.cs
@@ -12,9 +12,85 @@ namespace Demo0831_2
 {
     public partial class Form1 : Form
     {
+        string arithmetic;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+
+            int num1;
+            int num2;
+
+            /*
+             * Check if textboxes contain only numbers.
+             * Calculate if they do.
+             * 
+             * Show the results.
+             */
+
+            bool isNum = int.TryParse(tbNumber1.Text, out num1);
+            if (isNum)
+            {
+                isNum = int.TryParse(tbNumber2.Text, out num2);
+                if (isNum)
+                {
+                    int calc;
+
+                    switch (arithmetic)
+                    {
+                        case "add":
+                            calc = num1 + num2;
+                            break;
+                        case "sub":
+                            calc = num1 - num2;
+                            break;
+                        case "multi":
+                            calc = num1 * num2;
+                            break;
+                        case "div":
+                            calc = num1 / num2;
+                            break;
+                        default:
+                            calc = 0;
+                            break;
+                    }
+
+                    MessageBox.Show(calc.ToString());
+
+                }
+            }
+            if (!isNum)
+            {
+                MessageBox.Show("Please enter numbers in both boxes.");
+            }
+
+        }
+
+        /*
+         * Set calculation method.
+         */
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            arithmetic = "add";
+        }
+
+        private void btnSub_Click(object sender, EventArgs e)
+        {
+            arithmetic = "sub";
+        }
+
+        private void btnMulti_Click(object sender, EventArgs e)
+        {
+            arithmetic = "multi";
+        }
+
+        private void btnDiv_Click(object sender, EventArgs e)
+        {
+            arithmetic = "div";
         }
     }
 }
