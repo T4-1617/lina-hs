8f9f4c5fa948d2867d37aa2200a237ce73795ad2
lina-hs <lina.hannersanchez@gmail.com>
2016-09-02 12:20:54 +0200

    [Demo0902] Created class Ocean
    Ocean contains the grid size, grid, boat location and number of boats.

--- Demo0902/Demo0902/Program.cs
+++ Demo0902/Demo0902/Program.cs
@@ -9,12 +9,79 @@ namespace Demo0902
     class Program
     {
 
+        private static Random RNG;
+
+        class Ocean
+        {
+            public int SizeX = 0;
+            public int SizeY = 0;
+            public int Boats;
+            public char[,] Grid;
+
+            public Ocean(int GridSizeX, int GridSizeY)
+            {
+                SizeX = GridSizeX;
+                SizeY = GridSizeY;
+
+                CreateGrid();
+            }
+            
+            private void CreateGrid()
+            {
+
+                // Create the sea
+                for (int x = 0; x < SizeX; x++)
+                {
+                    for (int y = 0; y < SizeY; y++)
+                    {
+
+                        Grid[x, y] = 'O';
+
+                    }
+                }
+
+                // Create boats
+                Boats = RNG.Next(3, 6); // Decide between 3-5 boats
+                int CreatedBoats = 0;
+                while (CreatedBoats < Boats)
+                {
+                    // Choose random slot in grid
+                    int SlotX = RNG.Next(0, SizeX);
+                    int SlotY = RNG.Next(0, SizeY);
+
+                    // Check slot does not have a boat
+                    if (Grid[SlotX, SlotY] != 'B')
+                    {
+                        Grid[SlotX, SlotY] = 'B';
+                        CreatedBoats++;
+                    }
+                }
+
+            }
+            public void DisplayGrid()
+            { 
+                // Displays the grid on the screen
+                for (int x = 0; x < SizeX; x++)
+                {
+                    for (int y = 0; y < SizeY; y++)
+                    {
+                        Console.Write
+                        (
+                            string.Format
+                            (" {0} ",
+                            Grid[x, y].ToString())
+                        );
+                    }
+                }
+
+            }
+
+        }
 
         static void Main(string[] args)
         {
 
         }
-
 
     }
 }
