8ebdc61369c037d2e7348c4c44fc89c78bb6750b
lina-hs <lina.hannersanchez@gmail.com>
2016-09-07 09:51:05 +0200

    [BlackjackW36] Created class Deck and a RNG

--- BlackjackW36/BlackjackW36/Program.cs
+++ BlackjackW36/BlackjackW36/Program.cs
@@ -8,7 +8,43 @@ namespace BlackjackW36
 {
     class Program
     {
+        static Random RNG = new Random();
 
+        class Deck
+        {
+            List<Card> deck = new List<Card>();
+            int suits = 4;
+            int minCardNumber = 1;
+            int maxCardNumber = 13;
+
+            public Deck()
+            {
+
+            }
+            public void AddCardsToDeck()
+            {
+                // For each suit create one card for each number
+                for (int suit = 0; suit < suits; suit++)
+                {
+                    for (int number = minCardNumber; number <= maxCardNumber; number++)
+                    {
+                        deck.Add(new Card(suit, number));
+                    }
+                }
+            }
+            public void ClearDeck()
+            {
+                deck.Clear();
+            }
+            public Card DrawRandomCard()
+            {
+                // Take a random card from the deck
+                int cardPos = RNG.Next(0, deck.Count);
+                Card card = deck[cardPos];
+                deck.RemoveAt(cardPos);
+                return card;
+            }
+        }
         class Card
         {
             public int suit;
