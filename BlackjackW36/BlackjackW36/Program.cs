e86e7965dae7bd01c5647e62b66ec9874a36ed84
lina-hs <lina.hannersanchez@gmail.com>
2016-09-07 09:49:54 +0200

    [BlackjackW36] Created class card

--- BlackjackW36/BlackjackW36/Program.cs
+++ BlackjackW36/BlackjackW36/Program.cs
@@ -8,28 +8,7 @@ namespace BlackjackW36
 {
     class Program
     {
-        class Blackjack
-        {
-            public List<Card> deck = new List<Card>();
-            int suits = 4;
-            int minCardNumber = 1;
-            int maxCardNumber = 13;
 
-            public Blackjack()
-            {
-
-            }
-            void AddCardsToDeck()
-            {
-                for (int suit = 0; suit < suits; suit++)
-                {
-                    for (int number = minCardNumber; number <= maxCardNumber; number++)
-                    {
-                        deck.Add(new Card(suit, number));
-                    }
-                }
-            }
-        }
         class Card
         {
             public int suit;
