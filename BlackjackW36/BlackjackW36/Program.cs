e3d45bee98ce998d18b041d329c5a134c5c7117b
lina-hs <lina.hannersanchez@gmail.com>
2016-09-07 09:36:20 +0200

    [BlackjackW36] Created project

---
+++ BlackjackW36/BlackjackW36/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackjackW36
+{
+    class Program
+    {
+        class Blackjack
+        {
+            public List<Card> deck = new List<Card>();
+            int suits = 4;
+            int minCardNumber = 1;
+            int maxCardNumber = 13;
+
+            public Blackjack()
+            {
+
+            }
+            void AddCardsToDeck()
+            {
+                for (int suit = 0; suit < suits; suit++)
+                {
+                    for (int number = minCardNumber; number <= maxCardNumber; number++)
+                    {
+                        deck.Add(new Card(suit, number));
+                    }
+                }
+            }
+        }
+        class Card
+        {
+            public int suit;
+            public int number;
+
+            public Card(int cardSuit, int cardNumber)
+            {
+                suit = cardSuit;
+                number = cardNumber;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
