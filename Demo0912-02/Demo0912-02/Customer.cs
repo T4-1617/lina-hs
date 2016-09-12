05382ea18668146e060a087937cf0e67369885e1
lina-hs <lina.hannersanchez@gmail.com>
2016-09-12 11:29:20 +0200

    [Demo0912-02] Added the Customer class
    Added the Customer class with the customer properties firstname,
    lastname, active and ID.

---
+++ Demo0912-02/Demo0912-02/Customer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo0912_02
+{
+    class Customer
+    {
+        /// <summary>
+        /// Contains the first name of the customer (string)
+        /// </summary>
+        public string FirstName { get; set; }
+        /// <summary>
+        /// Contains the last name of the customer (string)
+        /// </summary>
+        public string LastName { get; set; }
+        /// <summary>
+        /// Contains the customer id (integer)
+        /// </summary>
+        public int ID { get; set; }
+        /// <summary>
+        /// Contains a value indicating the active state of the customer (bool)
+        /// </summary>
+        public int Active { get; set; }
+    }
+}
