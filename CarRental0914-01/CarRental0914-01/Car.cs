b3c1038bc201f88d2bc9f994ec30aefea6a6b6e6
lina-hs <lina.hannersanchez@gmail.com>
2016-09-14 11:57:09 +0200

    [CarRental0914-01] Added class Car

---
+++ CarRental0914-01/CarRental0914-01/Car.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRental0914_01
+{
+    class Car
+    {
+        public string make { get; set; }
+        public string RegNO { get; set; }
+        public bool hired { get; set; }
+        public string group { get; set; }
+
+        public Car()
+        {
+            make = "unknown";
+            RegNO = "unique";
+            hired = false;
+            group = "unknown";
+        }
+    }
+}
