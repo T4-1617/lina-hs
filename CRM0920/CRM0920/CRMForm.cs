2741a97b284356fb20b0f88b038575ae12a3d210
lina-hs <lina.hannersanchez@gmail.com>
2016-09-20 13:22:59 +0200

    [CRM0920] Edited default visibility of controllers

--- CRM0920/CRM0920/CRMForm.cs
+++ CRM0920/CRM0920/CRMForm.cs
@@ -15,6 +15,35 @@ namespace CRM0920
         public CRMForm()
         {
             InitializeComponent();
+
+            // Hide panels
+            pnlList.Visible = false;
+            pnlAdd.Visible = false;
+
+            // Hide extra arg labels
+            lblAddExtraArg1.Visible = false;
+            lblAddExtraArg2.Visible = false;
+            lblAddExtraArg3.Visible = false;
+            lblEditExtraArg1.Visible = false;
+            lblEditExtraArg2.Visible = false;
+            lblEditExtraArg3.Visible = false;
+
+            // Hide extra arg textboxes
+            tbxAddExtraArg1.Visible = false;
+            tbxAddExtraArg2.Visible = false;
+            tbxAddExtraArg3.Visible = false;
+            tbxEditExtraArg1.Visible = false;
+            tbxEditExtraArg2.Visible = false;
+            tbxEditExtraArg3.Visible = false;
+
+            // Disable all visible textboxes
+            tbxAddFName.Enabled = false;
+            tbxAddLName.Enabled = false;
+            tbxAddPhoneNum.Enabled = false;
+            tbxEditFName.Enabled = false;
+            tbxEditLName.Enabled = false;
+            tbxEditPhoneNum.Enabled = false;
+
         }
     }
 }
